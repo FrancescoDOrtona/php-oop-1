3b845a21662346d276dda928b1eb852cb14ce7fc
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-29 15:41:28 +0100

    Add: getter & setter Fn to movie & series

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,5 +1,36 @@
 <?php
 
 class Movie extends Production {
-    
+
+    public $profit;
+    public $duration;
+
+    public function __construct(string $img, string $title, string $language, int $rating, int $profit, int $duration) {
+        parent::__construct($img, $title, $language, $rating);
+        $this->profit = $profit;
+        $this->duration = $duration;
+    }
+    public function getProfit(){
+        return $this->profit;
+    }
+    public function getDuration(){
+        return $this->duration;
+    }
+
+    public function setProfit($profit){
+        if(is_numeric($profit) && $profit > 0){
+        $this->profit = $profit;
+    } else {
+        throw new Exception("Profit value not valid");
+    }
+
+    }
+
+    public function setDuration($duration){
+        if(is_numeric($duration) && $duration > 0){
+            $this->duration = $duration;
+        } else {
+            throw new Exception("Duration value not valid");
+        }
+    }
 }
--- Models/Serie.php
+++ Models/Serie.php
@@ -1,5 +1,24 @@
 <?php
 
 class Serie extends Production {
-    
+
+    public $season;
+
+    public function __construct(string $img, string $title, string $language, int $rating, $season) {
+        parent::__construct($img, $title, $language, $rating);
+        $this->season = $season;
+    }
+
+    public function getSeason(){
+        return $this->season;
+    }
+
+    public function setSeason($season){
+        if(is_numeric($season) && $season > 0){
+            $this->season = $season;
+        } else {
+            throw new Exception("Season value not valid");
+        }
+    }
+
 }
